cbfbb84a60835ce173662e889511f54a0db4553f
KennethZhu26 <kennethzhu09xd@gmail.com>
2025-06-14 18:48:38 +1200

    Update code1.py

--- code1.py
+++ code1.py
@@ -1,5 +1,6 @@
 #Shopping Cart Program - V1
 
+#Dictionary of valiable items
 available_items = {
     "1kg apple": 3.99,
     "1kg banana": 2.99,
@@ -8,13 +9,16 @@ available_items = {
     "1kg orange": 6.99
 }
 
+#Empty list waiting for items to be entered
 grocery_list = []
 
+#Print statement to welcome user
 print('-------------------------------------')
 print("Welcome user, please enter your name")
 name = input("Enter your name : ")
 print('-------------------------------------')
 
+#While loop asking user for their input
 while True:
     print("\n--- Grocery Shopping Menu ---")
     print("1. Add item")
@@ -24,28 +28,40 @@ while True:
     print("-----------------------------")
     choice = input("Choose an option (1-4): ")
 
+    #If user decides to add item
     if choice == "1":
+        #Prints avaliable items from grocery list
         print("\nAvailable items:")
+        #Prints item and price to 2 decimal points
         for item, price in available_items.items():
             print(f"- {item} (${price:.2f})")
         item = input("Enter item to add: ")
+        #Adds item to list
         grocery_list.append(item)
         print(f"'{item}' has been added to your list.")
 
+    #If user decides to view grocery list
     elif choice == "2":
+        #Prints user's current grocery list
         print("\nYour grocery list:")
+        #Sets total to 0
         total = 0
+        #Give index of the item starting from 1
         for i, item in enumerate(grocery_list, 1):
+            #Calculate price of cart
             price = available_items[item]
             total += price
             print(f"{i}. {item} - ${price:.2f}")
         print(f"Total: ${total:.2f}")
 
+    #If user decides to remove item
     elif choice == "3":
         item = input("Enter item to remove: ")
+        #Removes item
         grocery_list.remove(item)
         print(f"'{item}' has been removed.")
 
+    #If user decides to exit program
     elif choice == "4":
         print("Goodbye!")
         break
